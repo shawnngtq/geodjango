2bddb0cfc3e82f49b525c4ba4e667547d459765b
anonymous <shawn.coding.acc@gmail.com>
2020-05-31 11:15:15 +0800

    Update admin to Geographic Admin

--- world/admin.py
+++ world/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+
+from .models import WorldBorder
 
 # Register your models here.
+admin.site.register(WorldBorder, admin.GeoModelAdmin)
+
+"""https://code.djangoproject.com/ticket/31611"""
+# admin.site.register(WorldBorder, admin.OSMGeoAdmin)
